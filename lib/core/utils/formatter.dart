1f5d2ee622dc7512bdbe5dc00211132bcc84b8bc
TobiCrackIT <oluwatoyinoluwatobig@gmail.com>
2022-01-10 07:27:24 +0100

    fix decimal display

--- lib/core/utils/formatter.dart
+++ lib/core/utils/formatter.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 class Formatter {
   static toMoney(String value) {
     var mFmt = NumberFormat("#,##0.00", "en_US");
-    mFmt.minimumFractionDigits = 0;
+    //mFmt.minimumFractionDigits = 0;
     mFmt.maximumFractionDigits = 2;
     return mFmt.format(double.tryParse(value) ?? 0);
   }
