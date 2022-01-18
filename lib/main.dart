23d222afda50ce5f82b1eb3589d9ee20910f6eeb
TobiCrackIT <oluwatoyinoluwatobig@gmail.com>
2022-01-18 10:43:17 +0100

    add test

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ Future<void> main() async {
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,49 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:eve/eve.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:moovy/ui/views/login/login_view.dart';
+import 'package:moovy/core/services/local_storage_service.dart';
+import 'package:moovy/main.dart';
+import 'package:moovy/ui/widgets/loader.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Widgets smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(LoginView());
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          localStorageServiceProvider.overrideWithValue(MockLocalStorageService())
+        ],
+        child: Home(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that widgets are rendered
+    expect(find.byType(EveInputField), findsNWidgets(2));
+    expect(find.byType(EveButton), findsOneWidget);
+    expect(find.text('Login'), findsOneWidget);
+    expect(find.text('Forgot Password?'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    //Verify that loader is not shown at default state
+    expect(find.byType(Loader), findsNothing);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+     //Verify that nothing happens when the login button is tapped at default state
+     await tester.tap(find.byType(EveButton));
+     await tester.pump();
+
   });
+}
+
+class MockLocalStorageService implements LocalStorageService{
+  @override
+  Future<void> setToken(String token) {
+    throw UnimplementedError();
+  }
+
+  @override
+  SharedPreferences get sharedPreferences => throw UnimplementedError();
+
+  @override
+  String get token => throw UnimplementedError();
+
 }
